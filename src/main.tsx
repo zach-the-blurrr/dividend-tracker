f8faf738bdd8c98ba8bfd5fa4763d2a142716d92
zach-the-blurrr <debuhr.zach@gmail.com>
2026-02-15 11:46:30 -0600

    add recoil, downgrade react to 18 for recoil compatability, make a light/dark mode toggle to test recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </StrictMode>,
-)
+);
