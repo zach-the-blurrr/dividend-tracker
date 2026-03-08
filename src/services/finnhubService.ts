dd3b03ccb795468e796de4a8bfdcd057e1b283b5
zach-the-blurrr <debuhr.zach@gmail.com>
2026-03-08 17:10:08 -0500

    create an interface for the quote endpoint and implement it

--- src/services/finnhubService.ts
+++ src/services/finnhubService.ts
@@ -1,7 +1,9 @@
+import type { QuoteResponse } from "../types/QuoteResponse";
+
 const API_KEY = import.meta.env.VITE_FINNHUB_API_KEY;
 const BASE_URL = 'https://finnhub.io/api/v1/';
 
-export async function getQuote(ticker: string) {
+export async function getQuote(ticker: string): Promise<QuoteResponse | null> {
     console.log("API KEY:", API_KEY);
   const url = `${BASE_URL}quote?symbol=${ticker}&token=${API_KEY}`;
 
