9859f3ff907c1ff026ebdd83ba04339e79fbe091
zach-the-blurrr <debuhr.zach@gmail.com>
2026-03-08 16:38:49 -0500

    add untracked file

---
+++ src/services/finnhubService.ts
@@ -0,0 +1,18 @@
+const API_KEY = import.meta.env.VITE_FINNHUB_API_KEY;
+const BASE_URL = 'https://finnhub.io/api/v1/';
+
+export async function getQuote(ticker: string) {
+    console.log("API KEY:", API_KEY);
+  const url = `${BASE_URL}quote?symbol=${ticker}&token=${API_KEY}`;
+
+  try {
+    const result = await fetch(url);
+    if (!result.ok) { 
+        throw new Error(`Finnhub error: ${result.status}`);
+    }
+    return await result.json();
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+    return null;
+  }
+}
