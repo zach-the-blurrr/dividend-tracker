4212954f2a34cd6d32cf1bdff79bbb61b2bdd025
zach-the-blurrr <debuhr.zach@gmail.com>
2026-03-11 10:38:49 -0500

    remove logging of api key, add .env files to .gitignore

--- src/services/finnhubService.ts
+++ src/services/finnhubService.ts
@@ -4,7 +4,6 @@ const API_KEY = import.meta.env.VITE_FINNHUB_API_KEY;
 const BASE_URL = 'https://finnhub.io/api/v1/';
 
 export async function getQuote(ticker: string): Promise<QuoteResponse | null> {
-    console.log("API KEY:", API_KEY);
   const url = `${BASE_URL}quote?symbol=${ticker}&token=${API_KEY}`;
 
   try {
